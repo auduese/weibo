2aad147a5a770223810426248fcbf05a8c203f79
auduese <18300230330@163.com>
2020-10-28 08:59:20 +0000

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,17 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
     public function create()
     {
         return view('users.create');
@@ -20,7 +31,6 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
-
         $this->validate($request,[
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
@@ -40,11 +50,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit',compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             'name' => 'required|max:50',
             'password' => 'nullable|confirmed|min:6'
@@ -55,7 +67,6 @@ class UsersController extends Controller
         if($request->password){
             $data['password'] = bcrypt($request->password);
         }
-
         $user->update($data);
 
         session()->flash('success','个人资料更新成功！');
